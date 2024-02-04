514d4fa3b5c695bf49ed3655ff4a0b94b16bc81d
marcostenoriodev <marcosvtenoriodev@gmail.com>
2024-02-04 16:35:35 -0300

    change form create diet style

--- src/components/FormGenerateDiet.tsx
+++ src/components/FormGenerateDiet.tsx
@@ -2,10 +2,11 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog";
 import { Button } from "./ui/button";
-import Input from "./Input";
+import { Input, CheckboxUserTerms }from "./Input";
 
 const FormGenerateDiet = (props: any) => {
   const initialValues = {
+    dietName: "",
     username: "",
     height: "",
     weight: "",
@@ -17,10 +18,12 @@ const FormGenerateDiet = (props: any) => {
     indispensableFoods: "",
     objective: "",
     lives: "",
+    userTerms: false,
   };
 
   const validationSchema = Yup.object({
-    username: Yup.string().required("Campo obrigatório"),
+    dietName: Yup.string().required("Nome da dieta é obrigatório"),
+    username: Yup.string().required("Nome é obrigatório"),
     height: Yup.number().required("Campo obrigatório"),
     weight: Yup.number().required("Campo obrigatório"),
     age: Yup.number().required("Campo obrigatório"),
@@ -31,6 +34,7 @@ const FormGenerateDiet = (props: any) => {
     indispensableFoods: Yup.string().required("Campo obrigatório"),
     objective: Yup.string().required("Campo obrigatório"),
     lives: Yup.string().required("Campo obrigatório"),
+    userTerms: Yup.boolean().isTrue("Termos de uso é obrigatório"),
   });
 
   const onSubmit = (values: any) => {
@@ -41,51 +45,64 @@ const FormGenerateDiet = (props: any) => {
     <Dialog>
       <DialogTrigger>
         <div className="flex items-center gap-2 text-sm bg-primary text-primary-foreground shadow hover:bg-primary/90 justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4">
-        {props.children}
+          {props.children}
         </div>
       </DialogTrigger>
-      <DialogContent className="bg-nutriBlue w-72 sm:w-96 h-5/6 lg:w-full rounded-xl border-2 border-primary xl:min-w-max xl:h-auto">
-
+      <DialogContent className="bg-nutriBlue w-11/12 sm:w-10/12 h-5/6 lg:w-full rounded-xl border-2 border-primary xl:min-w-max xl:h-auto">
+        <h2 className="font-primary text-2xl textPurple font-bold">
+        Vamos lá !
+        </h2>
+        <h3 className="font-primary textPurple opacity-70 text-md">
+        Mas antes, precisamos de algumas informações suas.
+        </h3>
         <div className="overflow-auto flex justify-center">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          <Form className="flex flex-col w-56 sm:w-[310px] lg:w-[790px]">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form className="flex flex-col w-full p-2">
+              <div className="w-full flex flex-col xl:flex-row xl:gap-6">
+                <div className="w-full xl:w-6/12 flex flex-col justify-between">
+                  <Input control="dietName">Diet Name</Input>
+                  <Input control="username">Username</Input>
+                  <div className="flex gap-4 justify-between">
+                    <div className="w-5/12 flex flex-col gap-0">
+                      <Input control="height">height</Input>
+                      <Input control="weight">weight</Input>
+                    </div>
+                    <div className="w-5/12 flex flex-col gap-0">
+                      <Input control="age">age</Input>
+                      <Input control="gender">gender</Input>
+                    </div>
+                  </div>
 
-            <div className="w-full flex flex-col xl:flex-row xl:gap-6 py-8">
-              <div className="w-full xl:w-6/12 flex flex-col justify-between">
-                <Input control="username">Username</Input>
-                <div className="flex gap-4 justify-center">
-                  <div className="w-5/12 flex flex-col gap-4">
-                    <Input control="height">height</Input>
-                    <Input control="weight">weight</Input>
-                  </div>
-                  <div className="w-5/12 flex flex-col gap-4">
-                    <Input control="age">age</Input>
-                    <Input control="gender">gender</Input>
-                  </div>
+                  <Input control="biotype">biotype</Input>
                 </div>
 
-                <Input control="biotype">biotype</Input>
+                <div className="w-full xl:w-6/12">
+                  {" "}
+                  <Input control="activityFrequence">activityFrequence</Input>
+                  <Input control="foodRestriction">foodRestriction</Input>
+                  <Input control="indispensableFoods">indispensableFoods</Input>
+                  <Input control="objective">objective</Input>
+                  <Input control="lives">lives</Input>
+                </div>
+              </div>
+              <div className="flex items-center mb-4 pt-3">
+                <CheckboxUserTerms control='userTerms' >asdasdasd</CheckboxUserTerms>
               </div>
 
-              <div className="w-full xl:w-6/12">
-                {" "}
-                <Input control="activityFrequence">activityFrequence</Input>
-                <Input control="foodRestriction">foodRestriction</Input>
-                <Input control="indispensableFoods">indispensableFoods</Input>
-                <Input control="objective">objective</Input>
-                <Input control="lives">lives</Input>
+              <div className="w-full flex justify-center">
+                <Button
+                  type="submit"
+                  className="min-w-44 md:text-base xl:text-xl xl:p-5"
+                >
+                  Enviar
+                </Button>
               </div>
-            </div>
-
-            <div className="w-full flex justify-center">
-              <Button type="submit" className="min-w-44 md:text-base xl:text-xl xl:p-5">Enviar</Button>
-            </div>
-          </Form>
-        </Formik>
+            </Form>
+          </Formik>
         </div>
       </DialogContent>
     </Dialog>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,54 @@
-import { Field, ErrorMessage } from "formik"
+import { Field, ErrorMessage } from "formik";
 
-const Input = (props : any) => {
+const Input = (props: any) => {
   return (
     <div className="flex flex-col">
-    <label htmlFor={props.control} className={`${props.className} font-primary font-semibold mb-1"`}>{props.children}</label>
-    <Field type="text" id={props.control} name={props.control} className="border-2 border-primary h-10 rounded-lg"/>
-    <ErrorMessage name={props.control} component="p" className='text-red-500 font-medium'/>
-  </div>
-  )
-}
+      <label
+        htmlFor={props.control}
+        className={`${props.className} font-primary font-semibold mb-1"`}
+      >
+        {props.children}
+      </label>
+      <Field
+        type="text"
+        id={props.control}
+        name={props.control}
+        className={
+          props.fieldClassName
+            ? props.fieldClassName
+            : "border-2 border-primary h-10 rounded-lg"
+        }
+      />
+      <ErrorMessage
+        name={props.control}
+        component="p"
+        className="text-red-500 font-medium"
+      />
+    </div>
+  );
+};
 
-export default Input
+const CheckboxUserTerms = (props: any) => {
+  return (
+    <div className="flex flex-col w-full items-start">
+      <div className="flex gap-2">
+      <Field
+        type="checkbox"
+        id={props.control}
+        name={props.control}
+        className=""
+      />
+      <p>
+                  Li e aceito os{" "}
+                  <a href="" className="font-semibold underline">
+                    termos de uso e condições
+                  </a>
+                </p>
+      </div>
+      
+      <ErrorMessage name={props.control} component="p" className='text-red-500 font-medium'/>
+    </div>
+  );
+};
+
+export { Input, CheckboxUserTerms };
